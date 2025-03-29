0dad3bb570f5f62dabec0d84e008adc69b17fb9f
Lucas Albuquerque <guinho.lucas7@gmail.com>
2025-03-29 17:19:05 -0300

    add comments

--- prototype.py
+++ prototype.py
@@ -1,18 +1,22 @@
+# Importação de bibliotecas e inputs
 import numpy as np
 
 name_file = input('Nome do arquivo: ')
 
 def prototype(name):
+# Leitura do dataset e associação a uma matriz, e também sua impressão
     mat = np.loadtxt('grafos_datasets/' + name + '.txt')
     print(mat)
 
-    i, j = mat.shape
+# Acessando e imprimindo as dimensões da matriz
+    i, j = mat.shape 
 
     print(i)
     print(j)
 
+# Criação/abertura de um arquivo que armazenará a saída
     with open('outputs/' + name + '_output.txt', 'w') as file:
          file.write(name + ' ' + str(i) + ' ' + str(j))
 
-
+# Chamadas das funções
 prototype(name_file)
